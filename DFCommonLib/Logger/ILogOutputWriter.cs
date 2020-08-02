00172339202387eca22c122a07ad36af7ec524c2
Thor Richard Hanssen <thor.richard.hanssen@gmail.com>
2020-08-02 11:29:45 +0200

    Remove Server/DFCommonLib

--- DFCommonLib/Logger/ILogOutputWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace DFCommonLib.Logger
-{
-    public interface ILogOutputWriter
-    {
-        void LogMessage(DFLogLevel logLevel, string group, string message);
-        string GetName();
-    }
-}
