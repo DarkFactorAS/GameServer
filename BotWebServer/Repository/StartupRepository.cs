be0f777256b6d5c31d14c50f2c491856dc16231e
Thor Richard Hanssen <thor.richard.hansen@gmail.com>
2023-03-31 18:59:59 +0200

    Updated patcher to create the database properly

--- BotWebServer/Repository/StartupRepository.cs
+++ BotWebServer/Repository/StartupRepository.cs
@@ -22,10 +22,30 @@ namespace BotWebServer.Repository
         public bool RunPatcher()
         {
             _dbPatcher.Init();
-            _dbPatcher.Patch(PATCHER,3, "ALTER TABLE playfield ADD COLUMN version int(11) NOT NULL DEFAULT 7 AFTER boardSizeY");
-            _dbPatcher.Patch(PATCHER,4, "ALTER TABLE playfield ADD COLUMN revisionid int(11) NOT NULL DEFAULT 1 AFTER id");
+
+            // Create log table
+            _dbPatcher.Patch(PATCHER,1, "CREATE TABLE `logtable` ( `id` int(11) NOT NULL AUTO_INCREMENT, `created` datetime NOT NULL, `loglevel` int(11) NOT NULL, `groupname` varchar(100) NOT NULL DEFAULT '', `message` varchar(1024) NOT NULL DEFAULT '', PRIMARY KEY (`id`))");
+
+            // Playfield table
+            _dbPatcher.Patch(PATCHER,2, "CREATE TABLE `playfield` ("
+            + " `id` int(11) NOT NULL AUTO_INCREMENT, " 
+            + " `revisionid` int(11) NOT NULL DEFAULT 1,"
+            + " `updated` datetime NOT NULL,"
+            + " `ownerid` varchar(30) NOT NULL DEFAULT '', "
+            + " `name` varchar(30) NOT NULL DEFAULT '', "
+            + " `description` varchar(128) NOT NULL DEFAULT '', "
+            + " `playfieldFlags` int(11) NOT NULL DEFAULT 0,"
+            + " `numPlayers` int(11) NOT NULL DEFAULT 0,"
+            + " `numGoals` int(11) NOT NULL DEFAULT 0,"
+            + " `boardSizeX` int(11) NOT NULL DEFAULT 0,"
+            + " `boardSizeY` int(11) NOT NULL DEFAULT 0,"
+            + " `version` int(11) NOT NULL DEFAULT 1,"
+            + " `data` mediumblob DEFAULT NULL, "
+            + " PRIMARY KEY (`id`)"
+            + ")"
+            );
+
             return _dbPatcher.Successful();
         }
-
     }
 }
